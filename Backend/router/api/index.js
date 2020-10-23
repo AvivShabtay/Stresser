a0069b79e2d7ff8e31c795827a2ff53a370f650d
Aviv <shabtay.aviv@gmail.com>
2020-10-23 22:29:41 +0300

    Setup yarn packages & Client project
    * feat: Add simple client project
    * feat: Open client & server packages
    * fix: package.json files
    * feat: Add tailwind & postcss

--- Backend/router/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-router.use("/hosts", require("./hostsRouter"));
-router.use("/users", require("./usersRouter"));
-
-module.exports = router;
--- Backend/router/api/usersRouter.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const UsersController = require("../../controllers/UserController");
-
-router.get("/:id", UsersController.getUserById);
-
-module.exports = router;
