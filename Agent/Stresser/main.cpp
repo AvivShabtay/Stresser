3bf1ff5bb7a9329a14c544f095e2fb1b2a731025
Aviv Shbaty <shabtay.aviv@gmail.com>
2020-10-30 13:01:47 +0200

    Remove files from Stresser project

--- Agent/Stresser/main.cpp
+++ Agent/Stresser/main.cpp
@@ -7,34 +7,12 @@
 #include "Request.h"
 #include "Policy.h"
 
-/*
-#include "FakeArtifact.h"
-#include "FakeRegistry.h"
-#include "FakeFile.h"
-#include "ArtifactArgs.h"
-#include "ArtifactType.h"
-*/
-
 #pragma comment (lib, "winhttp.lib")
 
 using json = nlohmann::json;
 
 int __cdecl main(int argc, char** argv) {
-	/*
-	std::vector<FakeArtifact*> fakeArtifacts;
-
-	ArtifactType type1 = ArtifactType::Registry;
-	ArtifactArgs args1;
-	fakeArtifacts.push_back(new FakeRegistry(type1, args1));
-
-	ArtifactType type2 = ArtifactType::File;
-	ArtifactArgs args2;
-	fakeArtifacts.push_back(new FakeFile(type2, args2));
-
-	// Cleanup:
-	for (int i = 0; i < fakeArtifacts.size(); i++)
-		delete fakeArtifacts.at(i);
-	*/
+	// TODO
 }
 
 void testPolicyFromServer() {
