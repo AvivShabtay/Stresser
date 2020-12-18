be9372407af352397c5b138492671eae07fb49a7
Aviv Shabtay <shabtay.aviv@gmail.com>
2020-12-18 23:47:19 +0200

    Move all ArtifactManager test to the same file (#10)

--- Agent/Tests/TestArtifactManager.cpp
+++ Agent/Tests/TestArtifactManager.cpp
@@ -24,9 +24,96 @@ namespace Tests
 		return new FakeFile(artifactType, artifactArguments);
 	}
 
-	TEST_CLASS(TestArtifactManager2)
+	TEST_CLASS(TestArtifactManager)
 	{
 	public:
+
+		TEST_METHOD(AddArtifactsToArtifactManager)
+		{
+			/* Arrange */
+			ArtifactManager manager;
+			const int numOfArtifacts = 3;
+			ArtifactArgs argsArray[numOfArtifacts];
+			ArtifactType::Type typesArray[numOfArtifacts] = {
+				ArtifactType::Type::File,
+				ArtifactType::Type::Registry,
+				ArtifactType::Type::File
+			};
+
+			/* Act */
+			// Add artifacts to the manager:
+			for (int i = 0; i < numOfArtifacts; ++i) {
+				auto args = argsArray[i];
+				auto type = typesArray[i];
+
+				switch (type) {
+				case ArtifactType::Type::File:
+				{
+					auto artifact = new FakeFile(type, args);
+					manager.AddArtifact(artifact);
+					break;
+				}
+				case ArtifactType::Type::Registry:
+				{
+					auto artifact = new FakeRegistry(type, args);
+					manager.AddArtifact(artifact);
+					break;
+				}
+				default:
+					break;
+				}
+			}
+
+			/* Assert */
+			Assert::AreEqual(manager.Size(), numOfArtifacts);
+		}
+
+		TEST_METHOD(GetArtifactsFromArtifactManager)
+		{
+			/* Arrange */
+			ArtifactManager manager;
+			const int numOfArtifacts = 3;
+			ArtifactArgs argsArray[numOfArtifacts];
+			ArtifactType::Type typesArray[numOfArtifacts] = {
+				ArtifactType::Type::File,
+				ArtifactType::Type::Registry,
+				ArtifactType::Type::File
+			};
+
+			// Use for assert latter:
+			FakeArtifact* artifacts[numOfArtifacts];
+
+			/* Act */
+			// Add artifacts to the manager:
+			for (int i = 0; i < numOfArtifacts; ++i) {
+				auto args = argsArray[i];
+				auto type = typesArray[i];
+
+				switch (type) {
+				case ArtifactType::Type::File:
+				{
+					auto artifact = new FakeFile(type, args);
+					artifacts[i] = artifact;
+					manager.AddArtifact(artifact);
+					break;
+				}
+				case ArtifactType::Type::Registry:
+				{
+					auto artifact = new FakeRegistry(type, args);
+					artifacts[i] = artifact;
+					manager.AddArtifact(artifact);
+					break;
+				}
+				default:
+					break;
+				}
+			}
+
+			/* Assert */
+			auto artifact2 = manager.GetArtifactByIndex(2);
+			Assert::IsNotNull(artifact2);
+			Assert::IsTrue(artifact2 == artifacts[2]);
+		}
 
 		TEST_METHOD(ArtifactManagerInstall)
 		{
