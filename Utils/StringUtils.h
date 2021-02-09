db3ed47ec781c4330e6a31ec7827fa77d1685a44
Aviv <shabtay.aviv@gmail.com>
2021-02-09 22:22:05 +0200

    Update utils function
    * feat: Add more helper functions
    * fix: Update string comparison logic

--- Utils/StringUtils.h
+++ Utils/StringUtils.h
@@ -6,6 +6,15 @@ class StringUtils final
 {
 public:
 	static std::wstring RemoveQuotationMarks(const std::wstring data);
+
 	static std::string RemoveQuotationMarks(const std::string data);
+
+	static std::wstring RemoveNewLine(const std::wstring data);
+
+	static std::string RemoveNewLine(const std::string data);
+
+	static bool DoesEmptyJSON(const std::wstring data);
+
+	static bool DoesEmptyJSON(const std::string data);
 };
 
