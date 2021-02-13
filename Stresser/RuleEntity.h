698b518138c324315a5609f580fbd5637e94836f
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-02-13 16:23:33 +0200

    Update rule entity class
    *feat: Add default c'tor
    *feat: Add convert to/from functions

--- Stresser/RuleEntity.h
+++ Stresser/RuleEntity.h
@@ -1,17 +1,26 @@
 #pragma once
 #include <iostream>
 
+#include "nlohmann/json.hpp"
+
+using Json = nlohmann::json;
+
 class RuleEntity
 {
+public:
+	RuleEntity();
 	RuleEntity(std::string id, std::string name, std::string type, std::string data);
 	~RuleEntity();
-
-	friend std::ostream& operator << (std::ostream& out, const RuleEntity& ruleEntity);
 
 	const std::string GetID();
 	const std::string GetName();
 	const std::string GetType();
 	const std::string GetData();
+
+	static RuleEntity ConvertFromJson(Json jsRule);
+	static Json ConvertFromEntity(RuleEntity ruleEntity);
+
+	friend std::ostream& operator << (std::ostream& out, const RuleEntity& ruleEntity);
 
 private:
 	std::string m_id;
