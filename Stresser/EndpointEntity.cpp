48c0437f7f612d461c47e59cdfa79d749c45a831
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-02-13 15:24:56 +0200

    Update endpoint entity class
    * feat: Implement default c'tor
    * feat: Add policy ID member & getter function
    * feat: Add convert to/from functions

--- Stresser/EndpointEntity.cpp
+++ Stresser/EndpointEntity.cpp
@@ -1,8 +1,11 @@
 #include "EndpointEntity.h"
 
+EndpointEntity::EndpointEntity()
+	: m_id(""), m_hostname(""), m_ipAddress(""), m_apiKey(""), m_policyId(""), m_status("") { }
+
 EndpointEntity::EndpointEntity(std::string id, std::string hostname, std::string ipAddress, std::string apiKey,
-	std::string status)
-	: m_id(id), m_hostname(hostname), m_ipAddress(ipAddress), m_apiKey(apiKey), m_status(status) { }
+	std::string policyId, std::string status)
+	: m_id(id), m_hostname(hostname), m_ipAddress(ipAddress), m_apiKey(apiKey), m_policyId(policyId), m_status(status) { }
 
 EndpointEntity::~EndpointEntity() { }
 
@@ -26,6 +29,11 @@ const std::string EndpointEntity::GetAPIKey()
 	return this->m_apiKey;
 }
 
+const std::string EndpointEntity::GetPolicyID()
+{
+	return this->m_policyId;
+}
+
 const std::string EndpointEntity::GetStatus()
 {
 	return this->m_status;
@@ -44,6 +52,31 @@ void EndpointEntity::SetIPAddress(const std::string ipAddress)
 void EndpointEntity::GetStatus(const std::string status)
 {
 	this->m_status = status;
+}
+
+EndpointEntity EndpointEntity::ConvertFromJson(Json jsEndpoint)
+{
+	return EndpointEntity(
+		jsEndpoint["endpointId"].dump(),
+		jsEndpoint["hostname"].dump(),
+		jsEndpoint["IPAddress"].dump(),
+		jsEndpoint["apiKey"].dump(),
+		jsEndpoint["policyId"].dump(),
+		jsEndpoint["status"].dump()
+	);
+}
+
+Json EndpointEntity::ConvertFromEntity(EndpointEntity entity)
+{
+	Json jsEndpoint;
+	jsEndpoint["endpointId"] = entity.GetID();
+	jsEndpoint["hostname"] = entity.GetHostname();
+	jsEndpoint["IPAddress"] = entity.GetIPAddress();;
+	jsEndpoint["apiKey"] = entity.GetAPIKey();
+	jsEndpoint["policyId"] = entity.GetPolicyID();
+	jsEndpoint["status"] = entity.GetStatus();
+
+	return jsEndpoint;
 }
 
 std::ostream& operator<<(std::ostream& out, const EndpointEntity& endpointEntity)
