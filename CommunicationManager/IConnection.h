1c028a858f7ba9606960b2f7131edd6976b75223
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-02-13 15:21:30 +0200

    Adding interface for connection
    * feat: Define pure abstract class as interface

---
+++ CommunicationManager/IConnection.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+
+#include "nlohmann/json.hpp"
+
+/* Used as the JSON data parser. */
+using json = nlohmann::json;
+
+class IConnection
+{
+public:
+	IConnection() { }
+
+	/*
+	*
+	*/
+	virtual void SetServerURL(std::wstring serverURL) = 0;
+
+	/*
+
+	*/
+	virtual void SetToken(std::string token) = 0;
+
+	/*
+
+	*/
+	virtual json SendRequest(std::wstring requestType, std::wstring path, json data) = 0;
+
+protected:
+	std::wstring serverURL;
+	std::string m_token;
+};
