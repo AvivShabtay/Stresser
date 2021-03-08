95212234c0913c9367fe17fad9a199f942ef3ffa
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-03-08 23:56:05 +0200

    Add token communication manager
    * feat: Implement HTTP request using authorization 
    * feat: Implement worker thread to refresh tokens

---
+++ Stresser/AuthorizedHttpRequest.cpp
@@ -0,0 +1,94 @@
+#include "AuthorizedHttpRequest.h"
+
+#include "../Utils/AutoCriticalSection.h"
+#include "../Utils/ShutdownSignal.h"
+#include "../Utils/StringUtils.h"
+#include "../Utils/DebugPrint.h"
+
+#include <Windows.h>
+
+
+AuthorizedHttpRequest& AuthorizedHttpRequest::getInstance(const ServerDetails& server)
+{
+	static AuthorizedHttpRequest g_tokenManager(server);
+	return g_tokenManager;
+}
+
+AuthorizedHttpRequest::AuthorizedHttpRequest(const ServerDetails& server)
+	: m_server(server)
+{
+}
+
+void AuthorizedHttpRequest::refreshToken()
+{
+	const std::string targetPath("/endpoint/" + m_endpointId);
+
+	Json jsEndpointData;
+	jsEndpointData["id"] = this->m_endpointId;
+	jsEndpointData["apiKey"] = this->m_token;
+
+	Json jsResponse = this->sendRequest(http::verb::put, targetPath, jsEndpointData);
+
+	if (jsResponse.empty())
+	{
+		throw std::runtime_error("Server return with no data");
+	}
+
+	const std::string newToken = StringUtils::RemoveQuotationMarks(jsResponse["apiKey"].dump());
+	DEBUG_PRINT("[+] Changing token from: " + this->m_token + ", to: " + newToken);
+	this->setToken(newToken);
+}
+
+void AuthorizedHttpRequest::setToken(const std::string& newToken)
+{
+	AutoCriticalSection autoCriticalSection;
+	this->m_token = newToken;
+}
+
+bool AuthorizedHttpRequest::startTokenRefresherThread(const std::string& endpointId, const std::string& initialToken)
+{
+	this->m_endpointId = endpointId;
+	this->m_token = initialToken;
+
+	auto threadStartRoutine = [](auto params) {
+
+		auto tokenManager = reinterpret_cast<AuthorizedHttpRequest*>(params);
+
+		ShutdownSignal& g_signal = ShutdownSignal::GetInstance();
+		if (!g_signal.Get())
+		{
+			throw std::runtime_error("Could not get handle to shutdown event");
+		}
+
+		while (WAIT_TIMEOUT == WaitForSingleObject(g_signal.Get(), 10000))
+		{
+			tokenManager->refreshToken();
+		}
+
+		return static_cast<DWORD>(0);
+	};
+
+	this->m_ahTokenRefresherThread.reset(
+		CreateThread(nullptr, 0, threadStartRoutine, this, 0, nullptr));
+
+	if (INVALID_HANDLE_VALUE == this->m_ahTokenRefresherThread.get())
+	{
+		throw std::runtime_error("Could not create thread for refreshing tokens");
+	}
+
+	return true;
+}
+
+Json AuthorizedHttpRequest::sendRequest(const http::verb& requestMethod, const std::string& target, const Json& payload) const
+{
+	if (this->m_token.empty())
+	{
+		return HttpRequest::sendRequest(this->m_server.getUserAgent(), this->m_server.gethttpVersion(), this->m_server.getContentType(), "",
+			requestMethod, this->m_server.getHostname(), target, this->m_server.getPort(), payload);
+	}
+	else
+	{
+		return HttpRequest::sendRequest(this->m_server.getUserAgent(), this->m_server.gethttpVersion(), this->m_server.getContentType(),
+			this->m_token, requestMethod, this->m_server.getHostname(), target, this->m_server.getPort(), payload);
+	}
+}
---
+++ Stresser/AuthorizedHttpRequest.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "ServerDetails.h"
+#include "HttpRequest.h"
+#include "../Utils/AutoHandle.h"
+
+#include "nlohmann/json.hpp"
+
+#include <boost/beast/http.hpp>
+
+#include <iostream>
+
+using Json = nlohmann::json;
+
+/*
+ * Used to provide communication with the application server by managing unique token
+ * representing authorized endpoint.
+ */
+class AuthorizedHttpRequest
+{
+public:
+	static AuthorizedHttpRequest& getInstance(const ServerDetails& server);
+
+	/*
+	 * Start sending request to the server to keep the endpoint token valid.
+	 * @param endpointId - Unique ID of the endpoint that received from the server.
+	 * @param initialToken - Token value that was received from the server before.
+	 */
+	bool startTokenRefresherThread(const std::string& endpointId, const std::string& initialToken);
+
+	/*
+	 * Simplified HTTP request that automatically inject the valid token value.
+	 * @param requestMethod - Boost	type represent request type.
+	 * @param target - Represent the URI path in the server.
+	 * @param payload - The data to be send in the HTTP request.
+	 */
+	Json sendRequest(const http::verb& requestMethod, const std::string& target, const Json& payload) const;
+
+private:
+	/* Keep class as singleton. */
+	AuthorizedHttpRequest(const ServerDetails& server);
+
+	/*
+	 * Send HTTP request the expected arguments to the server for refreshing the
+	 * endpoint token.
+	 * @note Using Endpoint data such as ID.
+	 */
+	void refreshToken();
+
+	/*
+	 * Thread safe setter for token value.
+	 * @param newToken - The new token value.
+	 */
+	void setToken(const std::string& newToken);
+
+	ServerDetails m_server;
+	std::string m_endpointId;
+	std::string m_token;
+	AutoHandle m_ahTokenRefresherThread;
+	std::thread m_refreshTokenThread;
+};
+
