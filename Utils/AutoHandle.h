d1956c7061ae52ec567ed3a82706bdce405f0dcd
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-04-13 21:14:13 +0300

    Remove redundant exception type

--- Utils/AutoHandle.h
+++ Utils/AutoHandle.h
@@ -1,30 +1,11 @@
 #pragma once
 #include "pch.h"
+#include <memory>
+#include <Windows.h>
 
 struct HandleDeleter
 {
-	void operator()(const HANDLE& handle)
-	{
-		if (handle)
-		{
-			CloseHandle(handle);
-		}
-	}
+	void operator()(const HANDLE& handle) const;
 };
 
-struct WinHttpHandleDeleter
-{
-	void operator()(const HINTERNET& handle)
-	{
-		if (handle)
-		{
-			WinHttpCloseHandle(handle);
-		}
-	}
-};
-
-/* RAII wrapper for HANDLEs. */
 using AutoHandle = std::unique_ptr<void, HandleDeleter>;
-
-/* RAII wrapper for HINTERNET handle. */
-using AutoHttpHandle = std::unique_ptr<void, WinHttpHandleDeleter>;
