08dc173f1cc9545f7c729b16dfe5c120973d5ca1
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-05-14 17:55:32 +0300

    Add event object
    * feat: Open handle to existing event
    * feat: Manage operations on the event object

---
+++ StresserEngine/Event.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "DriverConfiguration.h"
+#include "GenericHandle.h"
+#include "Value.h"
+
+class Event final
+{
+public:
+	Event();
+
+	explicit Event(StresserString eventName);
+
+	~Event();
+
+	Event(Event&& other) noexcept;
+	Event& operator=(Event&& other) noexcept;
+
+	Value<bool, NTSTATUS> open(StresserString eventName);
+
+	Value<bool, NTSTATUS> close() const;
+
+	Value<bool, NTSTATUS> set() const;
+
+	void release();
+
+	const HANDLE& get() const;
+
+	const StresserString& getEventName() const;
+
+private:
+	HANDLE m_event;
+	StresserString m_eventName;
+};
+
