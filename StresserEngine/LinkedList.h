557956a867432d37f9f29a20caacfeac7c3309c3
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-05-16 11:38:28 +0300

    Update linked list
    * feat: Add documentation
    * feat: Fix issues with types

---
+++ StresserEngine/LinkedList.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "AutoLock.h"
+#include "FastMutex.h"
+
+/*
+	Linked list data structure.
+	Accept allocated item and mange it under the linked list data structure.
+	@note Assuming item in the linked list contains field with LIST_ENTRY member named "Entry".
+	@note Make sure to free the memory.
+ */
+template<typename T, typename Lock = FastMutex>
+class LinkedList
+{
+public:
+	LinkedList()
+		: m_head{ nullptr, nullptr }
+	{
+		this->init();
+	}
+
+	/* Initialize the linked list and the private lock. */
+	void init()
+	{
+		InitializeListHead(&m_head);
+		m_lock.init();
+	}
+
+	/* Insert allocated item to tail of the linked list. */
+	void insertTail(T* item)
+	{
+		AutoLock locker(m_lock);
+		InsertTailList(&m_head, &item->Entry);
+	}
+
+	/* Insert allocated memory to the head of the linked list. */
+	void insertHead(T* item)
+	{
+		AutoLock locker(m_lock);
+		InsertHeadList(&m_head, &item->Entry);
+	}
+
+	/* Remove item from the linked list, without free it's memory. */
+	T* removeHead()
+	{
+		AutoLock locker(m_lock);
+		auto* const entry = RemoveHeadList(&m_head);
+		return CONTAINING_RECORD(entry, T, Entry);
+	}
+
+	/* Get the item in the head of the linked list. */
+	T* getHead()
+	{
+		AutoLock locker(m_lock);
+		auto* const entry = m_head.Flink;
+		return CONTAINING_RECORD(entry, T, Entry);
+	}
+
+private:
+	LIST_ENTRY m_head;
+	Lock m_lock;
+};
