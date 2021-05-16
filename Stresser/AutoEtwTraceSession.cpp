505d9bca720de78c8384624c75787fa405f41414
EladShriki <elad7575@gmail.com>
2021-05-16 14:23:41 +0300

    Change the EVENT_TRACE_PROPERTIES size Previous value cause a stack corruption

--- Stresser/AutoEtwTraceSession.cpp
+++ Stresser/AutoEtwTraceSession.cpp
@@ -65,7 +65,7 @@ void AutoEtwTraceSession::startTrace()
 	auto* properties = reinterpret_cast<PEVENT_TRACE_PROPERTIES>(this->m_propertiesBuffer.get());
 
 	// If exists stop the trace session with the same name
-	EVENT_TRACE_PROPERTIES prop = { 1024 };
+	EVENT_TRACE_PROPERTIES prop = { sizeof(EVENT_TRACE_PROPERTIES) };
 	ControlTrace(NULL, this->m_sessionName.c_str(), &prop, EVENT_TRACE_CONTROL_STOP);
 
 	ULONG error = ::StartTrace(&this->m_traceSessionHandle, this->m_sessionName.c_str(), properties);
