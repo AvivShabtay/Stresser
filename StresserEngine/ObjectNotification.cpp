92efd4b268dfc331401fc7ecf5e115bcd8cdeef7
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-05-17 20:42:38 +0300

    Implement Stresser process detection
    * feat: Implement detection logic
    * feat: Add data on the event
    * feat: Use registration context

--- StresserEngine/ObjectNotification.cpp
+++ StresserEngine/ObjectNotification.cpp
@@ -1,6 +1,8 @@
 #include "ObjectNotification.h"
 #include "DriverConfiguration.h"
 #include "ProcessUtils.h"
+#include "StresserEngine.h"
+#include "AutoLock.h"
 
 OB_PREOP_CALLBACK_STATUS StresserEngineObPreOpCallback(PVOID RegistrationContext, POB_PRE_OPERATION_INFORMATION OperationInformation)
 {
@@ -20,22 +22,86 @@ void StresserEngineObPostOpCallback(PVOID RegistrationContext, POB_POST_OPERATIO
 		return;
 	}
 
-	auto* const process = static_cast<PEPROCESS>(OperationInformation->Object);
-	auto processPid = ProcessUtils::getPidFromProcess(process);
+	// Get driver's specific context:
+	auto* notificationContext = static_cast<PNotificationContext>(RegistrationContext);
+	if (nullptr == notificationContext)
+	{
+		return;
+	}
 
-	if (processPid.isError())
+	// Get data corresponding to the requested process:
+	auto* const requestedProcess = static_cast<PEPROCESS>(OperationInformation->Object);
+	auto requestedProcessPid = ProcessUtils::getPidFromProcess(requestedProcess);
+
+	if (requestedProcessPid.isError())
 	{
 		KdPrint((DRIVER_PREFIX "ObjectNotification invalid PID\n"));
 		return;
 	}
 
-	auto processFileName = ProcessUtils::getProcessFileNameByPid(processPid.getValue());
-	if (processFileName.isError())
+	bool doesFakeProcess = false;
+
 	{
-		KdPrint((DRIVER_PREFIX "ObjectNotification invalid filename\n"));
+		AutoLock lock(notificationContext->mutex);
+
+		auto* fakeProcessIds = notificationContext->fakeProcessIds;
+		if (nullptr == fakeProcessIds)
+		{
+			return;
+		}
+
+		for (ULONG i = 0; i < fakeProcessIds->size(); ++i)
+		{
+			const FakeProcessId* fakeProcessId = fakeProcessIds->getAt(i);
+			if (requestedProcessPid.getValue() == fakeProcessId->processId)
+			{
+				doesFakeProcess = true;
+			}
+		}
+	}
+
+	// Current process doesn't fake process:
+	if (!doesFakeProcess)
+	{
 		return;
 	}
 
-	const StresserString imageFileName = processFileName.getValue();
-	KdPrint((DRIVER_PREFIX "ObjectNotification PID=0x%08X, File name= %ws\n", processPid, imageFileName.get()));
+	// Create event item memory:
+	auto* eventItem = new (PagedPool, DRIVER_TAG) EventItem<EventInfo>;
+	if (nullptr == eventItem)
+	{
+		LOG_MESSAGE("Could not allocate memory for " STRINGIFY(EventItem));
+		return;
+	}
+
+	// Set event info:
+	auto* eventInfo = &eventItem->data;
+	KeQuerySystemTimePrecise(&eventInfo->time);
+	eventInfo->processId = HandleToULong(PsGetCurrentProcessId());
+	eventInfo->fakeProcessId = requestedProcessPid.getValue();
+
+	// Force null-terminated string:
+	memset(eventInfo->processName, 0, MAX_PROCESS_NAME_LENGTH + 1);
+
+	// Get filename path of the calling process:
+	auto processFileName = ProcessUtils::getProcessFileNameByPid(eventInfo->processId);
+	if (!processFileName.isError())
+	{
+		const StresserString filenamePath = processFileName.getValue();
+		wcscpy_s(eventInfo->processName, MAX_PROCESS_NAME_LENGTH, filenamePath.get());
+	}
+
+	KdPrint((DRIVER_PREFIX "PID=0x%08X open handle to fake process PID=0x%08X\n", eventInfo->processId, eventInfo->fakeProcessId));
+
+	{
+		AutoLock lock(notificationContext->mutex);
+
+		auto* fakeProcessEvents = notificationContext->fakeProcessEvents;
+		if (nullptr == fakeProcessEvents)
+		{
+			return;
+		}
+
+		fakeProcessEvents->insertTail(eventItem);
+	}
 }
