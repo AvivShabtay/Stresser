39bae243a2eab5f1276aa364900e4411f7589fd7
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-05-18 00:41:07 +0300

    Add standalone tester for the kernel driver interface
    * feat: Add interfaces for I\O communication
    * feat: Add main for CLI tester
    * feat: Change project settings for debugging purposes

--- SimpleMainTester/KernelDetector.cpp
+++ SimpleMainTester/KernelDetector.cpp
@@ -39,16 +39,76 @@ void KernelDetector::removeFakeProcessId(ULONG processId) const
 	}
 }
 
-/*
-void KernelDetector::registerEvent(HANDLE eventObject) const
+void KernelDetector::registerEvent(std::wstring eventName) const
 {
-	RegisterEvent registerEvent{ eventObject };
+	if (MAX_EVENT_NAME_LENGTH < eventName.length())
+	{
+		throw std::runtime_error("Invalid length of event name");
+	}
+
+	if (eventName.empty())
+	{
+		throw std::runtime_error("Invalid event name");
+	}
+
+	RegisterEvent registerEvent{ 0 };
+
+	// Make sure for null-terminator:
+	memset(registerEvent.eventName, 0, sizeof(WCHAR) * MAX_EVENT_NAME_LENGTH);
+
+	wcscpy_s(registerEvent.eventName, MAX_EVENT_NAME_LENGTH, eventName.c_str());
 
 	DWORD returnedBytes;
-	if (!DeviceIoControl(this->stresserDevice.get(), IOCTL_STRESSER_ENGINE_REMOVE_FAKE_PID, &registerEvent,
+	if (!DeviceIoControl(this->stresserDevice.get(), IOCTL_STRESSER_ENGINE_REGISTER_EVENT, &registerEvent,
 		REGISTER_EVENT_SIZE, nullptr, 0, &returnedBytes, nullptr))
 	{
 		throw Win32ErrorCodeException("Could not commit device control request");
 	}
 }
-*/
+
+EventsResult KernelDetector::receiveEvents(ULONG numberOfEvents) const
+{
+	if (numberOfEvents < 1)
+	{
+		throw std::runtime_error("Invalid number of events to receive");
+	}
+
+	auto eventsBuffer = std::make_unique<EventInfo[]>(numberOfEvents);
+	if (nullptr == eventsBuffer)
+	{
+		throw std::runtime_error("Could not allocate memory for events buffer");
+	}
+
+	// Zero all the memory:
+	memset(eventsBuffer.get(), 0, EVENT_INFO_SIZE * numberOfEvents);
+
+	// Prepare the request:
+	EventsHeader eventsHeader
+	{
+		numberOfEvents,
+		eventsBuffer.get()
+	};
+
+	DWORD returnedBytes;
+	if (!DeviceIoControl(this->stresserDevice.get(), IOCTL_STRESSER_ENGINE_GET_EVENTS, nullptr, 0, &eventsHeader,
+		EVENTS_HEADER_SIZE, &returnedBytes, nullptr))
+	{
+		throw Win32ErrorCodeException("Could not commit device control request");
+	}
+
+	// There are no available events:
+	if (0 == returnedBytes)
+	{
+		return EventsResult{ 0, nullptr };
+	}
+
+	const int size = returnedBytes / EVENT_INFO_SIZE;
+
+	EventsResult result
+	{
+		size,
+		std::move(eventsBuffer)
+	};
+
+	return result;
+}
--- SimpleMainTester/Main.cpp
+++ SimpleMainTester/Main.cpp
@@ -1,20 +1,100 @@
+#include "KernelDetector.h"
+
 #include "../Utils/AutoHandle.h"
 #include "../Utils/DebugPrint.h"
 #include "../Utils/SehTranslatorGuard.h"
+#include "../Utils/Win32ErrorCodeException.h"
+#include "../Utils/StringUtils.h"
+#include "../Utils/TimeUtils.h"
 
 #include <exception>
+#include <iostream>
+#include <sstream>
+#include  <iomanip>
+
+#include <conio.h>
+#include <stdio.h>
 
 #include <Windows.h>
 
-int main() {
+const std::wstring EVENT_NAME(L"OnFakeProcess");
+const std::wstring UM_GLOBAL_FAKE_PROCESS_EVENT(L"Global\\" + EVENT_NAME);
+const std::wstring KM_FAKE_PROCESS_EVENT(L"\\BaseNamedObjects\\" + EVENT_NAME);
+const ULONG FAKE_PROCESS_ID_FOR_TESTING = 15;
 
-	//SehTranslatorGuard sehTranslatorGuard;
+void printUsage();
+std::string convertSystemTimeToString(const LARGE_INTEGER& time);
+void addFakeProcessId(ULONG processId);
+void removeFakeProcessId(ULONG processId);
+void registerNotificationEventAndWait();
+void consumeEvents();
+
+int main(int argc, PWCHAR argv[])
+{
+
+	SehTranslatorGuard sehTranslatorGuard;
+
+	if (argc < 2)
+	{
+		printUsage();
+		return -1;
+	}
 
 	try
 	{
+		const int choice = _wtoi(argv[1]);
 
-		DEBUG_PRINT("bla");
+		switch (choice)
+		{
+		case 1:
+		{
+			// Add fake process ID
+			if (argc < 3)
+			{
+				printUsage();
+				return -1;
+			}
 
+			const ULONG processId = atoi(reinterpret_cast<PCHAR>(argv[2]));
+			std::cout << "Process ID: " << processId << std::endl;
+
+			addFakeProcessId(processId);
+			break;
+		}
+		case 2:
+		{
+			// Remove fake process ID
+			if (argc < 3)
+			{
+				printUsage();
+				return -1;
+			}
+
+			const ULONG processId = atoi(reinterpret_cast<PCHAR>(argv[2]));
+			std::cout << "Process ID: " << processId << std::endl;
+
+			removeFakeProcessId(processId);
+			break;
+		}
+		case 3:
+		{
+			// Register notification event
+
+			registerNotificationEventAndWait();
+			break;
+		}
+		case 4:
+		{
+			// Get collected events
+
+			consumeEvents();
+			break;
+		}
+		default:
+		{
+			throw std::runtime_error("Invalid user choice");
+		}
+		}
 	}
 	catch (std::exception& exception)
 	{
@@ -23,4 +103,113 @@ int main() {
 	}
 
 	return 0;
+}
+
+void printUsage()
+{
+	std::cout << "Usage: SimpleMainTester.exe < 1 | 2 | 3 > [parameters]" << std::endl;
+	std::cout << "\t1 [pid] - add fake process ID" << std::endl;
+	std::cout << "\t2 [pid] - remove fake process ID" << std::endl;
+	std::cout << "\t3 - register notification event and wait for signal" << std::endl;
+	std::cout << "\t4 - consume events" << std::endl;
+}
+
+void addFakeProcessId(ULONG processId)
+{
+	const KernelDetector kernelDetector;
+	kernelDetector.addFakeProcessId(processId);
+
+	std::cout << "Add fake process ID: " << FAKE_PROCESS_ID_FOR_TESTING << std::endl;
+}
+
+void removeFakeProcessId(ULONG processId)
+{
+	const KernelDetector kernelDetector;
+	kernelDetector.removeFakeProcessId(processId);
+
+	std::cout << "Remove fake process ID: " << FAKE_PROCESS_ID_FOR_TESTING << std::endl;
+}
+
+void registerNotificationEventAndWait()
+{
+	const AutoHandle objectNotificationEvent(CreateEvent(nullptr, TRUE, FALSE, UM_GLOBAL_FAKE_PROCESS_EVENT.c_str()));
+	if (!objectNotificationEvent.get())
+	{
+		throw Win32ErrorCodeException("Could not create global event");
+	}
+
+	std::cout << "Event handle: " << std::hex << objectNotificationEvent.get() << std::endl;
+
+	const KernelDetector kernelDetector;
+	kernelDetector.registerEvent(KM_FAKE_PROCESS_EVENT);
+
+	if (WAIT_OBJECT_0 != WaitForSingleObject(objectNotificationEvent.get(), INFINITE))
+	{
+		throw Win32ErrorCodeException("Wait for signal from Driver failed");
+	}
+
+	consumeEvents();
+}
+
+void consumeEvents()
+{
+	const KernelDetector kernelDetector;
+
+	constexpr int NUMBER_OF_EVENTS_TO_CONSUME = 10;
+	const std::wstring TIME_FORMAT(L"%d/%m/%Y - %H:%M:%S");
+
+	const EventsResult eventsResult = kernelDetector.receiveEvents(NUMBER_OF_EVENTS_TO_CONSUME);
+	if (0 == eventsResult.size)
+	{
+		std::cout << "There are no available events" << std::endl;
+		return;
+	}
+
+	if (nullptr == eventsResult.events)
+	{
+		throw std::runtime_error("Invalid events buffer");
+	}
+
+	for (int i = 0; i < eventsResult.size; ++i)
+	{
+		const auto& eventInfo = eventsResult.events.get()[i];
+
+		std::wcout
+			<< "~~~~Event~~~~\n"
+			<< "Called PID=" << eventInfo.processId << "\n"
+			<< "Called Filename: " << eventInfo.processName << "\n"
+			<< "Fake process ID=" << eventInfo.fakeProcessId << "\n"
+			<< "Event time: " << TimeUtils::systemTimeToTimestamp(eventInfo.time, TIME_FORMAT) << "\n"
+			<< std::endl;
+	}
+}
+
+std::string convertSystemTimeToString(const LARGE_INTEGER& time)
+{
+	//SYSTEMTIME systemTime;
+	//FileTimeToSystemTime((FILETIME*)&time, &systemTime);
+
+	FILETIME localTime{ 0 };
+	FileTimeToLocalFileTime((FILETIME*)&time, &localTime);
+
+	SYSTEMTIME systemTime;
+	FileTimeToSystemTime(&localTime, &systemTime);
+
+	std::stringstream timeStream;
+	timeStream << std::setfill('0') << std::setw(2) << systemTime.wDay;
+	timeStream << "/";
+	timeStream << std::setfill('0') << std::setw(2) << systemTime.wMonth;
+	timeStream << "/";
+	timeStream << std::setfill('0') << std::setw(4) << systemTime.wYear;
+	timeStream << "-";
+	timeStream << std::setfill('0') << std::setw(2) << systemTime.wHour;
+	timeStream << ":";
+	timeStream << std::setfill('0') << std::setw(2) << systemTime.wMinute;
+	timeStream << ":";
+	timeStream << std::setfill('0') << std::setw(2) << systemTime.wSecond;
+	timeStream << ".";
+	timeStream << std::setfill('0') << std::setw(3) << systemTime.wMilliseconds;
+
+	std::string timeString(timeStream.str());
+	return timeString;
 }
