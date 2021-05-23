581de566495929ad4283a29e17eb7c0ab620340e
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-05-23 23:40:40 +0300

    Add macro for logging unicode messages

--- Utils/DebugPrint.h
+++ Utils/DebugPrint.h
@@ -12,6 +12,15 @@ do {									\
 #define DEBUG_PRINT(message)	// <-- Empty on purpose to prevent printing message in release mode
 #endif
 
+#ifdef _DEBUG
+#define DEBUG_WPRINT(message)			\
+do {									\
+  std::wcout << (message) << std::endl;	\
+} while (0)
+#else
+#define DEBUG_WPRINT(message)	// <-- Empty on purpose to prevent printing message in release mode
+#endif
+
  /*
 	 Return the token himself.
 	 https://stackoverflow.com/q/55636670
