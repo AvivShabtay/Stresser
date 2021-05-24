111d04a77a5a622d41dca8eb3359fce9a29d0b3b
Aviv Shabtay <shabtay.aviv@gmail.com>
2021-05-24 21:43:11 +0300

    Fix bug with Artifact manager (#43)

--- Stresser/IArtifactSubscriber.cpp
+++ Stresser/IArtifactSubscriber.cpp
@@ -5,7 +5,7 @@ void IArtifactSubscriber::reset()
 	this->m_artifactsToReport.clear();
 }
 
-void IArtifactSubscriber::setNewArtifacts(const std::vector<IArtifact*>& artifacts)
+void IArtifactSubscriber::setNewArtifacts(const std::vector<std::shared_ptr<IArtifact>>& artifacts)
 {
 	this->m_artifactsToReport = artifacts;
 }
