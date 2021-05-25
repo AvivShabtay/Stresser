b7278c659b75e4a1280836edcc3a2ba89fee44b6
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-05-26 00:19:15 +0300

    Fix bags & add protection

--- StresserEngine/Vector.h
+++ StresserEngine/Vector.h
@@ -14,15 +14,19 @@ public:
 	explicit Vector(const ULONG capacity = 4)
 		: m_items(nullptr), m_size(0), m_capacity(capacity)
 	{
-		this->m_items = allocate(capacity);
+		this->m_items = this->allocate(capacity);
 		this->m_lock.init();
 	}
 
 	~Vector()
 	{
-		if (nullptr != this->m_items)
+		__try
 		{
-			ExFreePoolWithTag(this->m_items, Tag);
+			this->free();
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER)
+		{
+			KdPrint(("Exception was thrown in Vector destructor\n"));
 		}
 	}
 
@@ -35,7 +39,7 @@ public:
 	/*
 	 * Return the number of items in the vector.
 	 */
-	ULONG size() const
+	const ULONG size() const
 	{
 		return this->m_size;
 	}
@@ -53,8 +57,6 @@ public:
 	 */
 	void add(const T& item)
 	{
-		NT_ASSERT(this->m_size <= this->m_capacity);
-
 		AutoLock<FastMutex> lock(this->m_lock);
 
 		if (this->m_size == this->m_capacity)
@@ -69,21 +71,32 @@ public:
 	/*
 	 * Return item by it's index.
 	 */
-	T& getAt(const size_t index)
+	T getAt(const ULONG index)
 	{
-		NT_ASSERT(index <= this->m_size);
+		AutoLock<FastMutex> lock(this->m_lock);
 
-		return this->m_items[index];
+		if (this->isValidIndexCapacity(index))
+		{
+			return this->m_items[index];
+		}
+
+		return nullptr;
 	}
 
 	/*
 	 * Return reference to item by it's index.
 	 */
-	const T& getAt(const size_t index) const
+	const T getAt(const ULONG index) const
 	{
-		NT_ASSERT(index <= this->m_size);
+		AutoLock<FastMutex> lock(this->m_lock);
 
-		return this->m_items[index];
+		if (this->isValidIndexCapacity(index))
+		{
+			return this->m_items[index];
+		}
+
+
+		return nullptr;
 	}
 
 	/*
@@ -105,21 +118,27 @@ public:
 	/*
 	 * Set item by it's index.
 	 */
-	void setAt(size_t index, const T& item)
+	void setAt(ULONG index, const T& item)
 	{
-		NT_ASSERT(index <= this->m_size);
+		AutoLock<FastMutex> lock(this->m_lock);
 
-		this->m_items[index] = item;
+		if (this->isValidIndexSize(index))
+		{
+			this->m_items[index] = item;
+		}
 	}
 
 	/*
 	 * Remove item by it's index.
 	 */
-	void removeAt(size_t index)
+	void removeAt(ULONG index)
 	{
-		NT_ASSERT(index <= this->m_size);
-
 		AutoLock<FastMutex> lock(this->m_lock);
+
+		if (!this->isValidIndexSize(index))
+		{
+			return;
+		}
 
 		// The item is not the last item:
 		if (index < this->m_size - 1)
@@ -169,13 +188,32 @@ public:
 	 */
 	void resize(const ULONG capacity)
 	{
+		if (!this->isValidIndexCapacity(capacity))
+		{
+			return;
+		}
+
 		this->m_capacity = capacity;
 
 		T* items = this->allocate(this->m_capacity);
+		if (nullptr == items)
+		{
+			return;
+		}
 
 		memcpy(items, this->m_items, sizeof(T) * this->m_size);
 
 		this->m_items = items;
+	}
+
+	/* Release all the allocated memory and zero sizes. */
+	void clear()
+	{
+		AutoLock<FastMutex> lock(this->m_lock);
+
+		memset(this->m_items, 0, this->m_capacity);
+
+		this->m_size = 0;
 	}
 
 private:
@@ -184,14 +222,39 @@ private:
 	 */
 	T* allocate(const ULONG size)
 	{
-		auto buffer = static_cast<T*>(ExAllocatePoolWithTag(PoolType, sizeof(T) * size, Tag));
+		if (!this->isValidIndexCapacity(size))
+		{
+			return nullptr;
+		}
+
+		auto* buffer = static_cast<T*>(ExAllocatePoolWithTag(PoolType, sizeof(T) * size, Tag));
 		if (nullptr == buffer)
 		{
+			KdPrint(("Could not resize the vector, invalid memory\n"));
 			return nullptr;
 		}
 
 		RtlZeroMemory(buffer, sizeof(T) * size);
 		return buffer;
+	}
+
+	/* Free all the allocated memory */
+	void free()
+	{
+		if (nullptr != this->m_items)
+		{
+			ExFreePoolWithTag(this->m_items, Tag);
+		}
+	}
+
+	bool isValidIndexCapacity(ULONG index)
+	{
+		return (0 <= index && index <= this->m_capacity);
+	}
+
+	bool isValidIndexSize(ULONG index)
+	{
+		return (0 <= index && index <= this->m_size);
 	}
 
 	T* m_items;			// Pointer to the items in the vector
