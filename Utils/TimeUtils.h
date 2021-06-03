f41be6274e28ade6ff672864c32ce1d469113b77
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-06-04 01:02:43 +0300

    Add helper to get current timestamp

--- Utils/TimeUtils.h
+++ Utils/TimeUtils.h
@@ -2,10 +2,17 @@
 
 #include <string>
 
+//std::string TIMESTAMP_FORMAT("%d-%m-%Y %H:%M:%S");
+
+/* https://www.boost.org/doc/libs/1_49_0/doc/html/date_time/date_time_io.html */
+//std::string SHORT_TIMESTAMP_FORMAT("%x %X");
+
 class TimeUtils final
 {
 public:
 	static std::string GetCurrentDateTime();
+
+	static std::string getCurrentTimestamp(std::string format = "%x %X");
 
 	/*
 		Convert system time value to local system time depending the given format.
