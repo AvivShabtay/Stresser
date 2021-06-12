37a6de4a2f8f0518ec2cca43eab38b1eb60c24bd
EladShriki <elad7575@gmail.com>
2021-06-12 21:33:43 +0300

    Move all Stresser stuff from the main to StresserApplication

---
+++ Stresser/StresserApplication.cpp
@@ -0,0 +1,87 @@
+#include "StresserApplication.h"
+#include "Controllers.h"
+#include "../Utils/EventsNames.h"
+
+StresserApplication::StresserApplication(const ServerDetails serverDetails) :
+	m_shutdownEvent(STOP_STRESSER)
+{
+	// Define communication object:
+	AuthorizedHttpRequest& authorizedHttpRequest = AuthorizedHttpRequest::getInstance(serverDetails, m_shutdownEvent.get());
+
+	this->initializeEndpoint(authorizedHttpRequest);
+
+	this->initializeDetectors(authorizedHttpRequest);
+}
+
+StresserApplication::~StresserApplication()
+{
+	this->StresserApplication::stop();
+}
+
+void StresserApplication::initializeEndpoint(AuthorizedHttpRequest& authorizedHttpRequest)
+{
+	// Define application controllers:
+	EndpointController& endpointController = EndpointController::getInstance(authorizedHttpRequest);
+	PolicyController& policyController = PolicyController::getInstance(authorizedHttpRequest);
+	RuleController& ruleController = RuleController::getInstance(authorizedHttpRequest);
+
+	// Register new endpoint in the server:
+	EndpointEntity endpoint = endpointController.createEndpoint();
+	std::string endpointId = endpoint.GetID();
+
+	// Start token manager:
+	authorizedHttpRequest.startTokenRefresherThread(endpointId, endpoint.GetAPIKey());
+
+	this->m_policyNotifications = std::make_unique<PolicyNotifications>(endpointId, this->m_shutdownEvent.get(), endpointController, policyController, ruleController);
+
+	this->m_policyNotifications->subscribe(this->m_artifactManager.get());
+}
+
+void StresserApplication::initializeDetectors(AuthorizedHttpRequest& authorizedHttpRequest)
+{
+	EventController& eventController = EventController::getInstance(authorizedHttpRequest);
+
+	UserModeDetector userModeDetector(eventController);
+	KernelDetector kernelDetector(eventController);
+
+	this->m_artifactManager->subscribe(&userModeDetector);
+	this->m_artifactManager->subscribe(&kernelDetector);
+
+	this->m_detectors.push_back(userModeDetector);
+	this->m_detectors.push_back(kernelDetector);
+}
+
+void StresserApplication::start()
+{
+	for (IStresserDetector& detector : this->m_detectors)
+	{
+		detector.start();
+	}
+}
+
+void StresserApplication::stop()
+{
+	this->m_shutdownEvent.setEvent();
+
+	for (IStresserDetector& detector : this->m_detectors)
+	{
+		detector.stop();
+	}
+}
+
+void StresserApplication::pause()
+{
+	for (IStresserDetector& detector : this->m_detectors)
+	{
+		detector.stop();
+	}
+}
+
+void StresserApplication::waitForShutdown()
+{
+	if (WAIT_OBJECT_0 != this->m_shutdownEvent.wait())
+	{
+		throw std::runtime_error("Unexpected error with the shutdown wait!");
+	}
+}
+
---
+++ Stresser/StresserApplication.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "IStresserApplication.h"
+#include "ServerDetails.h"
+#include "ArtifactManager.h"
+#include "PolicyNotifications.h"
+#include "KernelDetector.h"
+#include "UserModeDetector.h"
+
+class StresserApplication : public IStresserApplication
+{
+public:
+	explicit StresserApplication(const ServerDetails serverDetails);
+
+	~StresserApplication() override;
+
+	void start() override;
+
+	void stop() override;
+
+	void pause() override;
+
+	void waitForShutdown() override;
+
+private:
+	void initializeEndpoint(AuthorizedHttpRequest& authorizedHttpRequest);
+	void initializeDetectors(AuthorizedHttpRequest& authorizedHttpRequest);
+
+	WindowsEvent m_shutdownEvent;
+	std::unique_ptr<PolicyNotifications> m_policyNotifications;
+	std::unique_ptr<ArtifactManager> m_artifactManager;
+	std::vector<IStresserDetector> m_detectors;
+};
+
