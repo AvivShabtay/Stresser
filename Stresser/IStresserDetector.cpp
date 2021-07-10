a179955167cd2f78a911309ff72d6a66a3f4b86d
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-07-10 17:45:31 +0300

    Add handler for detectors in lost connection mode

--- Stresser/IStresserDetector.cpp
+++ Stresser/IStresserDetector.cpp
@@ -1,4 +1,7 @@
 #include "IStresserDetector.h"
+#include "InvalidResponseException.h"
+
+#include "../Utils/DebugPrint.h"
 
 IStresserDetector::IStresserDetector(const EventController& eventController)
 	:m_eventController(eventController)
@@ -7,6 +10,13 @@ IStresserDetector::IStresserDetector(const EventController& eventController)
 
 void IStresserDetector::sendEvent(EventEntity& eventEntity) const
 {
-	this->m_eventController.sendEvent(eventEntity);
+	try
+	{
+		this->m_eventController.sendEvent(eventEntity);
+	}
+	catch (const InvalidResponseException& exception)
+	{
+		DEBUG_TRACE(IStresserDetector, "Could not send event to server");
+	}
 }
 
